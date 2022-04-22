3202f5933441ae3ed933b352bec28ddcc92e733e
erayayasli <68869193+erayayasli@users.noreply.github.com>
2022-04-22 15:50:03 +0300

    Add files via upload

---
+++ so_long/derle.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc main.c is_map_rectangle.c is_wall_ok.c is_everything_ok.c -framework OpenGL -framework AppKit -lmlx -L./mlx
--- so_long/main.c
+++ so_long/main.c
@@ -1,10 +1,74 @@
 #include "so_long.h"
-#include <mlx.h>
-int main(int argc, char **argv)
+#include "./mlx/mlx.h"
+
+
+int	main(void)
 {
+	void *mlx;
+	void *window;
+	void *img;
+	void *duvar ="./img/duvar.xpm";
+	void *zemin ="./img/zemin.xpm";
+	void *oyuncu ="./img/oyuncu.xpm";
+	void *take = "./img/take.xpm";
+	void *exit = "./img/exit.xpm";
 
-    void *mlx;
+	int x;
+	int y;
+	int x1 = 0;
+	int y1 = 0;
 
-    mlx = mlx_init();
 
+
+	
+
+	
+	int fd ;
+    char *map;
+    char **splitmap;
+
+    fd = open("map.ber",O_RDONLY);
+    map = read_and_save(fd);
+	splitmap = ft_split(map,'\n');
+	
+	mlx = mlx_init();
+	window = mlx_new_window(mlx,1920,1080,"Planet");
+	
+	int i = 0;
+	int j = 0;
+	while(splitmap[i])
+	{	
+		while(splitmap[i][j])
+		{
+			if(splitmap[i][j] == '1')
+			{
+				img = mlx_xpm_file_to_image(mlx,duvar,&x,&y);
+				mlx_put_image_to_window(mlx,window,img,x1+(j*64),y1+(i*64));
+			}
+			else
+			{	
+				img = mlx_xpm_file_to_image(mlx,zemin,&x,&y);
+				mlx_put_image_to_window(mlx,window,img,x1+(j*64),y1+(i*64));
+			}
+			if(splitmap[i][j] == 'P')
+			{
+				img = mlx_xpm_file_to_image(mlx,oyuncu,&x,&y);
+				mlx_put_image_to_window(mlx,window,img,x1+(j*64),y1+(i*64));	
+			}
+			if(splitmap[i][j] == 'E')
+			{
+				img = mlx_xpm_file_to_image(mlx,exit,&x,&y);
+				mlx_put_image_to_window(mlx,window,img,x1+(j*64),y1+(i*64));	
+			}
+			if(splitmap[i][j] == 'C')
+			{
+				img = mlx_xpm_file_to_image(mlx,take,&x,&y);
+				mlx_put_image_to_window(mlx,window,img,x1+(j*64),y1+(i*64));	
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	mlx_loop(mlx);
 }
